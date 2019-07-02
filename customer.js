0ca6c3e9edda04a63b8f9d38a21f47eb61eac748
Brittany Taylor <hopkins.britt@gmail.com>
2019-07-01 22:38:33 -0500

    coded inquirer, but don't have db connected after all :(

--- customer.js
+++ customer.js
@@ -33,3 +33,79 @@ function displayItems() {
     });
 
 } askBuyer();
+
+// Function to ask the user if they want to purchase an item.
+function askBuyer() {
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "What product would you like to buy? (Please type the product ID)",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function (response) {
+            var itemID = response.id;
+            var itemQuantity = response.quantity;
+            connection.query("SELECT * FROM products WHERE ?", [{
+                item_id: itemID
+            }],
+                function (err, chosenItem) {
+                    if (err) throw err;
+                    if (chosenItem[0].stock_quantity - itemQuantity >= 0) {
+                        var total = itemQuantity * chosenItem[0].price;
+                        console.log("Your total purchase is $" + total + ".");
+                        connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", [chosenItem[0].stock_quantity - itemQuantity, itemID],
+                            function (err) {
+                                if (err) throw err;
+                                startOver();
+                            })
+                    } else {
+                        console.log("Order NOT completed.  We currently have" + chosenItem[0].stock_quanity + "of " + chosenItem[0].product_name + " in stock.");
+                        startOver();
+
+                    }
+
+                })
+
+        })
+
+}
+
+// Function to reset.
+function startOver() {
+    inquirer
+        .prompt([
+            {
+                name: "startOver",
+                type: "list",
+                choices: ["Yes", "No"],
+                message: "Would you like to place another order?"
+
+            }
+        ]).then(function (response) {
+            if (response.startOver === "Yes") {
+                displayItems();
+                askBuyer();
+            } else {
+                console.log("Thank you. Have a great day!")
+                connection.end();
+            }
+        })
+}
