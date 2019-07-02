07304e37f4ca387cd155093f34392d76534358f0
Brittany Taylor <hopkins.britt@gmail.com>
2019-07-01 21:45:31 -0500

    created mySQL database and table

--- customer.js
+++ customer.js
@@ -16,3 +16,20 @@ connection.connect(function (err) {
     displayItems();
 
 });
+
+// Function to display the product items in the terminal window.
+function displayItems() {
+    console.log("\nId" + " | " + "Product" + " | " + "Department" + " | " + "Price" + " | " + "Quantity")
+    console.log("====================================================");
+    
+// Retrieveing data from the database table.
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(
+                res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("====================================================");
+    });
+
+} askBuyer();
